0da96352b09081bb8dd50181f3823e7dad31ed78
kuriyama585 <kuriyama585@yahoo.co.jp>
2022-04-02 12:59:51 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      # 【翻訳】ポップアップを出す。⇨環境変数で設定したIDとパスワードを反映させてくだい。
+
+    end
+  end
 end
